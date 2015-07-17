46f10dfac9ac9d3588d1faf8ad5195ae35d131b0
Rahim Abdi <abdi.abdirahim@gmail.com>
2015-07-17 15:18:37 -0400

    update ReadME

--- script.js
+++ script.js
@@ -28,3 +28,33 @@ var fib = function(input)
 {
   return input < 2 ? input : fib(input - 1) + fib(input - 2);
 }
+
+var computeSumThree = function ()
+{
+  var inputValue = document.getElementById("inputThree").value;
+  var greatestPrimeFactorSoFar = 0;
+  if (inputValue < 2)
+  {
+      alert("No prime factors exist.");
+  }
+  else if (inputValue == 2)
+  {
+      alert("The greatest prime factor for '2' is 2.");
+  }
+  else {
+        for (var i = 1; i <= Math.sqrt(inputValue); i++)
+          {
+              if (inputValue % i == 0)
+                {
+                  greatestPrimeFactorSoFar = i;
+                }
+          }
+          if (greatestPrimeFactorSoFar == 1)
+          {
+             alert("The greatest prime factor is " + inputValue + ".");
+          }
+          else {
+              alert("The greatest prime factor is " + greatestPrimeFactorSoFar + ".");
+          }
+  }
+}
